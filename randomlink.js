51a4674ac4f8fe4ee8d1052c2c21bbdfef6bf19c
sdhoore <105303762+sdhoore@users.noreply.github.com>
2022-06-12 01:30:48 +0200

    Update randomlink.js

--- randomlink.js
+++ randomlink.js
@@ -2,6 +2,14 @@ var urls = new Array();
 urls[0] = "/spooky";
 urls[1] = "/anotherdayanotherworstebroodje";
 urls[2] = "/handsome";
+urls[] = "/";
+urls[] = "/";
+urls[] = "/";
+urls[] = "/";
+urls[] = "/";
+urls[] = "/";
+urls[] = "/";
+urls[] = "/";
 
 var random = Math.floor(Math.random()*urls.length);
 
